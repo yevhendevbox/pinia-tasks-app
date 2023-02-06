caa58bf20008288a015d0db5cbe76828e8d80bc7
Yevhen Dovhan <y.d.devbox@gmail.com>
2023-02-06 21:23:47 +0200

    feat: added acces to a store within vue application

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,28 @@
         alt="Main Logo">
       <h1>Pinia Tasks</h1>
     </header>
+    <!-- task list  -->
+    <div class="task-list">
+      <div v-for="(task, i) in taskStore.tasks"
+        :key="i">
+        <task-details :task="task" />
+      </div>
+    </div>
   </main>
 </template>
 
 <script>
+import TaskDetails from '@/components/TaskDetails.vue';
+import { useTaskStore } from '@/stores/TaskStore.js';
 export default {
+  components: {
+    TaskDetails,
+  },
+  setup() {
+    const taskStore = useTaskStore();
+
+    return { taskStore }
+  }
 
 }
 </script>
---
+++ src/components/TaskDetails.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="task">
+    <h3>{{ task.title }}</h3>
+    <div class="icons">
+      <i class="material-icons">delete</i>
+      <i class="material-icons">favorite</i>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Task Details",
+  props: {
+    task: {
+      required: true,
+      type: Object,
+    }
+  },
+  setup() {
+
+  }
+}
+</script>
